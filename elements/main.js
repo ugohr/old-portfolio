ed0f582ff345117c0d0daf61ce4bf4b68e5f126f
Ugo Hombourger <ugohbg@gmail.com>
2021-05-29 15:52:56 +0200

    [UPDATE] elements

--- elements/main.js
+++ elements/main.js
@@ -3,28 +3,30 @@ $(function () {
 	const ELEMENTS = ['🔥', '🍀', '⛰️', '⚡️', '💧'];
 
 	/* TYPES DE CARTES
-	 * 0 = CHIFFRE 75%
-	 * 1 = CHANGEMENT DE SENS 5%
-	 * 2 = PASSEMENT DE TOUR 7%
-	 * 3 = +2 6%
-	 * 4 = +4 3%
-	 * 5 = CHANGEMENT ELEMENTAIRE 4%
+	 * 0 = ÉLÉMENT 70%
+	 * 1 = CHANGEMENT DE SENS 6%
+	 * 2 = PASSEMENT DE TOUR 6%
+	 * 3 = +1 GÉNÉRAL 3%
+	 * 4 = +2 6%
+	 * 5 = +3 GÉNÉRAL 1%
+	 * 6 = +4 3%
+	 * 7 = +8 0.1%
+	 * 8 = CHANGEMENT ELEMENTAIRE 3%
+	 * 9 = MIROIR 1.4%
+	 * 10 = QUITTE OU DOUBLE 0.5%
 	*/
-	const TYPES = ['CHIFFRE', '⤾', 'Ø', '+2', '+4', '🔥/🍀/⛰️/⚡️/💧'];
+	const TYPES = ['', '🔄', '⛔️', '+1 GÉNÉRAL', '+2', '+3 GÉNÉRAL', '+4', '+8', '🔥🍀⛰️⚡️💧', '🪞', '🎲'];
 
 	$('#pioche').click(function () {
 
 		let cardType = getCardType();
 		let cardElement = '';
-		let cardNumber = 0;
 		if (cardType < 5)
-			cardElement = ELEMENTS[getIntBetween(0, 5)];
-		if (cardType === 0)
-			cardNumber = getIntBetween(0, 10);
+			cardElement = ELEMENTS[Math.floor(getIntBetween(0, 5))];
 
 		let card = document.createElement('div');
 		$(card).addClass('card '+cardElement.toLowerCase()).attr('onclick', 'this.parentNode.removeChild(this)');
-		$(card).append('<p class="card-value">'+ (cardType === 0 ? cardNumber : TYPES[cardType]) +'</p>');
+		$(card).append('<p class="card-value">'+ TYPES[cardType] +'</p>');
 		$(card).append('<p class="card-value">'+ cardElement +'</p>');
 
 		$('.cards').append($(card));
@@ -34,22 +36,32 @@ $(function () {
 	function getCardType() {
 		let n = getIntBetween(0, 100);
 
-		if (n < 75)
+		if (n < 70)
 			return 0
-		else if (n < 80)
+		else if (n < 76)
 			return 1
-		else if (n < 87)
+		else if (n < 82)
 			return 2
-		else if (n < 93)
+		else if (n < 85)
 			return 3
-		else if (n < 96)
+		else if (n < 91)
 			return 4
-		else if (n < 100)
+		else if (n < 92)
 			return 5
+		else if (n < 95)
+			return 6
+		else if (n < 95.1)
+			return 7
+		else if (n < 98.1)
+			return 8
+		else if (n < 99.5)
+			return 9
+		else if (n < 100)
+			return 10
 	}
 
 	function getIntBetween(min, max) {
-		return Math.floor(Math.random() * (max - min) + min);
+		return Math.random() * (max - min) + min;
 	}
 
 });
